0ac5da64b05c73e1e98774e89fd273257302b556
davinasct4044 <davinascimento4044@gmail.com>
2026-09-21 15:25:14 -0300

    feat: encapsulamento, construtor, controle de acesso de classe

--- src/main/java/com/queueflow/Main.java
+++ src/main/java/com/queueflow/Main.java
@@ -1,8 +1,30 @@
 package com.queueflow;
 
 public class Main {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Fila fila = new Fila();
+
+        Senha senha1 = new Senha();
+        Senha senha2 = new Senha();
+        Senha senha3 = new Senha();
+
+        fila.colocarSenhaEmEspera(senha1);
+        fila.colocarSenhaEmEspera(senha2);
+        fila.colocarSenhaEmEspera(senha3);
+
+        System.out.println("Aguarde...");
+
+        fila.chamarSenha();
+        fila.chamarSenha();
+
+        System.out.println(fila.espera);
+        System.out.println(fila.chamados);
+
+        fila.resetarFila();
+
+
+        System.out.println(fila.chamados);
+        System.out.println(fila.espera);
 
     }
 
--- src/main/java/com/queueflow/Senha.java
+++ src/main/java/com/queueflow/Senha.java
@@ -1,5 +1,16 @@
     package com.queueflow;
 
     public class Senha {
-        int senha;
+        private static int contador = 0;
+        private int senha;
+
+        public Senha(){
+            contador++;
+            senha = contador;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(senha);
+        }
     }
